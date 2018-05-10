5b35b39ee87fe91c8ecabb99c5e0a30f6506d91b
clairose <clairose@umich.edu>
2018-05-10 11:58:25 -0400

    New Javascript

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-let cashText = $("#cashPaid")
+$(document).ready(function(){
+    $(".receipt").hide();
+    let total = 100;
+    $(".total").text(`${total}`);
+    $("#submitcash").on("click", function () {
+    let paid = $("#cashPaid").val();
+    console.log(paid);
+    let change = (total - paid);
+    $(".change").text(`$${change}`);
+    });
+    $(".submit").on("click", function() {
+        $(".receipt").show();
+    })
+});
+
+class Cart {
+    constructor() {
+        this.cart = [];
+    }
+    add(n, p) {
+        let newItem = new Item(n, p);
+        this.cart.push(newItem);
+    }
+}
+
+class Item {
+    constructor(name, price) {
+        this.name = name;
+        this.price = price;
+    }
+}
