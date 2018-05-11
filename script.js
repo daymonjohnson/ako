c7f658e2653d68b4241e87ef32248dcb08c6594c
clairose <clairose@umich.edu>
2018-05-10 20:12:12 -0400

    Added subtotal function

--- script.js
+++ script.js
@@ -1,23 +1,5 @@
-$(document).ready(function(){
-    $(".receipt").hide();
-    let total = 100;
-    $(".total").text(`${total}`);
-    $("#submitcash").on("click", function () {
-    let paid = $("#cashPaid").val();
-    console.log(paid);
-    let change = (total - paid);
-    $(".change").text(`$${change}`);
-    });
-    $(".submitCash").on("click", function() {
-        $(".receipt").show();
-        $(".paymentType").text("cash.");
-    })
-    $(".submitCredit").on("click", function() {
-        $(".receipt").show();
-        $(".paymentType").text("card.");
-    })
-});
 
+let sum=0;
 class Cart {
     constructor() {
         this.cart = [];
@@ -26,16 +8,21 @@ class Cart {
         let newItem = new Item(n, p);
         this.cart.push(newItem);
         let i = this.cart.indexOf(newItem);
-
         let x = newItem.printName();
         let y = newItem.printPrice();
         $("th").before('<tr><td>'+x+'</td><td>$'+y+'</td></tr>');
-
-
+        this.calcSubtotal();
     }
     calcSubtotal() {
+        for(let b in this.cart) {
+            sum += this.cart[b].price;
+        }
+        console.log(sum);
+        $(".subTotal").text('$'+sum+'.00');
+        $(".total").text(sum+'.00');
     }
 }
+
 
 class Item {
     constructor(name, price) {
@@ -53,6 +40,25 @@ class Item {
 
 let c = new Cart;
 
+c.calcSubtotal();
+
+$(document).ready(function(){
+    $(".receipt").hide();
+    $("#submitcash").on("click", function () {
+    let paid = $("#cashPaid").val();
+    console.log(paid);
+    let change = paid - $(".total").text();
+    $(".change").text(`$${change}`);
+    });
+    $(".submitCash").on("click", function() {
+        $(".receipt").show();
+        $(".paymentType").text("cash.");
+    })
+    $(".submitCredit").on("click", function() {
+        $(".receipt").show();
+        $(".paymentType").text("card");
+    })
+});
 
 //span items and p tags fadein
 $(()=>{
